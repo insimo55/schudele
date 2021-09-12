e31db4579e5731899d3acc0df83aa1977376671d
Android2.0 <Korobeynkov.kick@gmail.com>
2021-09-12 11:01:44 +0500

    123

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let date = new Date();
 console.log(date.getDay());
-
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getHours()<12);
 
 if (date.getDay() == 1){
     document.getElementById("monday").classList.add("schedule__monday_active")
@@ -21,5 +23,22 @@ if (date.getDay() == 6){
     document.getElementById("saturday").classList.add("schedule__saturday_active")
 }
 if (date.getDay() == 0){
-    document.getElementById("sunday").classList.add("schedule__sunnday_active")
+    document.getElementById("sunday").classList.add("schedule__sunday_active")
 }
+
+if (date.getHours() >= 6 && date.getHours() <= 9){
+    document.getElementById("sun_1").classList.add("schedule__lession_active")
+}
+if (date.getHours() >= 9 && date.getHours() <= 11){
+    document.getElementById("sun_2").classList.add("schedule__lession_active")
+}
+if (date.getHours() >= 11 && date.getHours() <= 13){
+    document.getElementById("sun_3").classList.add("schedule__lession_active")
+}
+if (date.getHours() >= 13 && date.getHours() <= 15){
+    document.getElementById("sun_4").classList.add("schedule__lession_active")
+}
+
+
+
+
